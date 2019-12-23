55a27b1a8aa9269a30590826da3ccb5a4276591e
dhoangnvh <dhoangnvh@gmail.com>
2019-12-23 14:21:20 +0700

    update test

--- plugins/october/demo/components/Todo.php
+++ plugins/october/demo/components/Todo.php
@@ -18,7 +18,7 @@ class Todo extends ComponentBase
     {
         return [
             'max' => [
-                'description'       => 'The most amount of todo items allowed',
+                'description'       => 'The most amount of todo items allowed222',
                 'title'             => 'Max items',
                 'default'           => 10,
                 'type'              => 'string',
